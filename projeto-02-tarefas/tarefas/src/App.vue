4e6c95184c168852f5442286a8c45943ab74024c
Paulo França <paulofranca.contato@gmail.com>
2018-12-24 21:51:02 -0200

    criando components/TaskGrid.vue e components/Task.vue

--- projeto-02-tarefas/tarefas/src/App.vue
+++ projeto-02-tarefas/tarefas/src/App.vue
@@ -1,39 +1,45 @@
 <template>
   <div id="app">
     <h1>Tarefas</h1>
+		<TaskGrid :tasks="tasks"></TaskGrid>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskGrid from "./components/TaskGrid.vue";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  components: { TaskGrid },
+  data() {
+    return {
+      tasks: [
+        { name: "Lavar", pending: false },
+        { name: "Comprar blusa", pending: true }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-  body {
-		font-family: 'Lato', sans-serif;
-		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
-		color: #FFF;
-	}
+body {
+  font-family: "Lato", sans-serif;
+  background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+  color: #fff;
+}
 
-	#app {
-		display: flex;
-		flex: 1;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-	}
+#app {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
-	#app h1 {
-		margin-bottom: 5px;
-		font-weight: 300;
-		font-size: 3rem;
-	}
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
+}
 </style>
---
+++ projeto-02-tarefas/tarefas/src/components/Task.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="task" :class="stateClass">
+    <p>{{ task.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    task: { Type: Object, required: true }
+  },
+  computed: {
+    stateClass() {
+      return {
+        pending: this.task.pending,
+        done: !this.task.pending
+      };
+    }
+  }
+};
+</script>
+
+<style>
+.task {
+  box-sizing: border-box;
+  width: 250px;
+  height: 100px;
+  padding: 10px;
+  border-radius: 8px;
+  font-size: 1rem;
+  font-weight: 300;
+  cursor: pointer;
+  user-select: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.pending {
+  border-left: 12px solid #B73229;
+  background-color: #F44336;
+}
+.done {
+  color: #DDD;
+  border-left: 12px solid #0A8F08;
+  background-color: #4CAF50;
+}
+</style>
